80263807405b197b7ba5706c671948bbbf48936f
Tom Courtney <tpcourtney@gmail.com>
2017-01-11 13:21:06 -0500

    First commit
    Created basic shell and it runs

---
+++ Part1.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part1 {
+    public String FindSimpleGene(String dna){
+        
+    return("A");
+    }
+    
+    public void testSimpleGene() {
+    String dna = "AATGSGTAATTAATCG";
+    String gene = FindSimpleGene(dna);
+    System.out.println("Gene found is " + gene);
+  
+    }
+}
